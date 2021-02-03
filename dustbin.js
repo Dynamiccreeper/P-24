3bd9c4ad4b753a521a8719c8ccf3e565535b8745
Dynamiccreeper <74767888+Dynamiccreeper@users.noreply.github.com>
2021-02-03 16:28:12 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -13,6 +13,8 @@ class Dustbin{
     this.width = width;
     this.height = height;
     this.body = Bodies.rectangle(x,y,width,height,option);
+    this.image=loadImage("dustbingreen.png");
+    //this.image.scale=5;
     World.add(world,this.body);
     
     
@@ -23,14 +25,16 @@ class Dustbin{
             var  pos = this.body.position
            // var angle = this.body.angle;
             push();
-    
+            imageMode(CENTER);
+            image(this.image, pos.x, 300 + 10, 150, 150);
+          // image.scale=5;
             translate(pos.x,pos.y);
             //rotate(angle);
             rectMode(CENTER);
             strokeWeight(4);
             stroke('green');
             fill(255);
-            rect(0,0,this.width,this.height);
+            //rect(0,0,this.width,this.height);
     
             pop();
     
