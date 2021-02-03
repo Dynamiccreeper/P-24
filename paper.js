6a70592c5170f61ed9236616af9843d317bdeeec
Dynamiccreeper <74767888+Dynamiccreeper@users.noreply.github.com>
2021-02-03 16:29:28 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -6,11 +6,12 @@ class Paper{
     
    isStatic  : false,restitution : 0.3,friction:0.5,density:1.2
     }
-    
+
     this.x = x;
     this.y = y;
     this.r = r;
     this.body = Bodies.circle(  this.x,  this.y,  this.r/2,option);
+    this.image=loadImage("paper.png");
     World.add(world,this.body);
 
     
@@ -19,17 +20,20 @@ class Paper{
     
     display(){
     var  pos = this.body.position
-var angle  = this.body.position;
+var angle  = this.body.angle;
 //paper.body.position.x=mouseX
 //paper.body.position.y=mouseY
+imageMode(CENTER);
+image(this.image, pos.x, pos.y + 10, 50, 50);
    push()
+
    translate(pos.x,pos.y)
    rotate(angle);
-    imageMode(CENTER);
+
 
     strokeWeight(3);
     fill("purple");
-    ellipse(0,0,this.r,this.r);
+    //ellipse(0,0,this.r,this.r);
      pop()
     }
 }
