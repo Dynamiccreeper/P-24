eea2cf76ab6dbd8192a7c88e38ac8b340b690742
Dynamiccreeper <74767888+Dynamiccreeper@users.noreply.github.com>
2021-02-03 08:11:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,9 +22,9 @@ function setup() {
    // dustbin = new Dustbin (500,780,10,100);
 	ground = new Ground (600,380,1600,10);
 	paper = new Paper (200,250,50,50);
-	box1 = new Dustbin (500,360,100,10);
-  box2 = new Dustbin (550,360,10,100);
-	box3 = new Dustbin (600,360,10,100);
+	box1 = new Dustbin (550,380,100,10);
+  box2 = new Dustbin (500,330,10,100);
+	box3 = new Dustbin (600,330,10,100);
 
 }
 
@@ -33,15 +33,23 @@ function draw() {
 
   background(255);
   Engine.update(engine);
-
+  paper.x = World.mouseX;
+  paper.y = World.mouseY;
   ground.display();
   paper.display();
   box1.display();
   box2.display();
   box3.display();
-  drawSprites();
+ 
+ // drawSprites();
  
 }
 
+function keyPressed(){
+if(keyCode=== 38){
 
+  Matter.Body.applyForce(paper.body,paper.body.position,{x:80,y:-85});
+}
+
+}
 
